5b056898e5dace29600fbb421e81a82ff673e439
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-03 02:32:05 +0300

    Create redux store.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './components/App';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
