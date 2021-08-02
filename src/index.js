2d697bcf7ca4ba1d7fc9ec721867ed710e3ff62e
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-03 02:33:52 +0300

    Provide store to the application.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import rootReducer from './reducers/index';
@@ -8,7 +9,9 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
