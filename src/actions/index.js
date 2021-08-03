e08a24547f92e8b13dc448a7f62f0cfbf19c6687
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-04 01:56:31 +0300

    Add action creator for dispatching actions to fetch categories.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
+const api_key = process.env.REACT_APP_SECRET_KEY;
 
 export const fetchMeals = () => {
   return (dispatch) => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    axios.get(`https://www.themealdb.com/api/json/v1/${api_key}/filter.php?c=Seafood`)
       .then(({ data }) => {
         dispatch({ type: 'FETCH_MEALS', payload: data })
+      })
+  }
+};
+
+export const fetchCategories = () => {
+  return (dispatch) => {
+    axios.get(`https://www.themealdb.com/api/json/v1/${api_key}/categories.php`)
+      .then(({ data }) => {
+        dispatch({ type: 'FETCH_CATEGORIES', payload: data })
       })
   }
 };
