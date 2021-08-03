381e44d14bb99586d7b092d306294985450962fb
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-04 01:51:47 +0300

    Connect FilterRecipe component to redux store.

--- src/components/FilterRecipes.js
+++ src/components/FilterRecipes.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const categories = [];
-
-const FilterRecipes = () => {
+const FilterRecipes = (props) => {
+  const { categories } = props.categories
+  if (!categories) {
+    return(<div />)
+  }
   return (
     <div>
       <select name="categories" id="categories">
         <option value="All recipes">All recipes</option>
         {
           categories.map((category) =>(
-            <option key={category} value={category}>{category}</option>
+            <option key={category.strCategory} value={category.strCategory}>{category.strCategory}</option>
           ))
         }
       </select>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    categories: state.recipes.categories
+  }
+}
  
-export default FilterRecipes;
+export default connect(mapStateToProps)(FilterRecipes);
