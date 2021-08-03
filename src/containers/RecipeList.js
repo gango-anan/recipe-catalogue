f1faafdf8b3770dac541cbe9ae2112ec023fd4ad
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-03 13:42:49 +0300

    Remove unused code and display data from redux store.

--- src/containers/RecipeList.js
+++ src/containers/RecipeList.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const RecipeList = (props) => {
-  console.log(props.categories);
+const RecipeList = ({ categories }) => {
   return ( 
     <div>
-      <div>
-        <Link to="/chicken">Chicken</Link>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae voluptatum ducimus et voluptatem quos molestias ab laboriosam eveniet blanditiis magni nulla praesentium voluptate saepe quaerat, optio non. Obcaecati, accusamus?</p>
-      </div>
-      <div>
-        <Link to="/pork">Pork</Link>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae voluptatum ducimus et voluptatem quos molestias ab laboriosam eveniet blanditiis magni nulla praesentium voluptate saepe quaerat, optio non. Obcaecati, accusamus?</p>
-      </div>
-      <div>
-        <Link to="/seafood">Sea food</Link>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat beatae voluptatum ducimus et voluptatem quos molestias ab laboriosam eveniet blanditiis magni nulla praesentium voluptate saepe quaerat, optio non. Obcaecati, accusamus?</p>
-      </div>
+      {
+        categories.map((category) => {
+          const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } = category
+          return(
+            <div key={idCategory}>
+              <div>
+                <img src={strCategoryThumb} alt={strCategory} />
+              </div>
+              <Link to={`/${strCategory}`}>{strCategory}</Link>
+              <p>{ strCategoryDescription }</p>
+            </div>
+          )
+        })
+      }
     </div>
    );
 }
