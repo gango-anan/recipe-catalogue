e4c4af9b59be511f12df3cfcdfb5bb3044cdf53e
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-04 23:51:51 +0300

    Add styles to Recipe component.

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RecipeWrapper, MoreDetailsButton } from '../styles/styles';
 
 const Recipe = (props) => {
   const { category, imageUrl } = props;
   return (
-    <>
+    <RecipeWrapper>
+      <h3 style={{textAlign: "center", margin: "0.5rem 0"}}>{category.toUpperCase()}</h3>
       <div>
         <img src={imageUrl} alt={category} />
       </div>
-      <Link to={`/${category}`}>{category}</Link>
-    </>
+      <MoreDetailsButton type="button">
+        <Link to={`/${category}`}>See {category} meals</Link>
+      </MoreDetailsButton>
+    </RecipeWrapper>
   );
 }
  
