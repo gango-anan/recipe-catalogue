a8741090a94b684935209dad53a2bb61c4497976
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-06 13:15:57 +0300

    Move event listener logic in the FilterRecipe component into a function.

--- src/components/FilterRecipes.js
+++ src/components/FilterRecipes.js
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FilterSelector, FilterContainer } from '../styles/styles';
 
-const FilterRecipes = ({ categories, onChange }) => (
-  <FilterContainer>
-    Filter by category:
-    <FilterSelector name="categories" id="categories" onChange={(e) => onChange(e.target.value)}>
-      <option value="All recipes">All recipes</option>
-      {
-          categories.map((category) => (
-            <option key={category.strCategory} value={category.strCategory}>
-              {category.strCategory}
-            </option>
-          ))
-        }
-    </FilterSelector>
-  </FilterContainer>
-);
+const FilterRecipes = ({ categories, onChange }) => {
+  const handleOnChange = (e) => onChange(e.target.value);
+
+  return (
+    <FilterContainer>
+      Filter by category:
+      <FilterSelector name="categories" id="categories" onChange={handleOnChange}>
+        <option value="All recipes">All recipes</option>
+        {
+            categories.map((category) => (
+              <option key={category.strCategory} value={category.strCategory}>
+                {category.strCategory}
+              </option>
+            ))
+          }
+      </FilterSelector>
+    </FilterContainer>
+  );
+};
 
 FilterRecipes.propTypes = {
   onChange: PropTypes.func.isRequired,
